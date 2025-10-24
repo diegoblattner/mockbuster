8e76ca384f18dd299b761aba1fbf264d3414608a
Diego Blattner <>
2025-10-24 21:10:18 +0100

    img url removed double slash

--- packages/ui-lib/src/components/movie-card/movie-card.tsx
+++ packages/ui-lib/src/components/movie-card/movie-card.tsx
@@ -19,7 +19,7 @@ export function MovieCard({
 		<div className="movie-card">
 			<div className="movie-card__img">
 				<img
-					src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
+					src={`https://image.tmdb.org/t/p/w200${poster_path}`}
 					alt={title}
 					loading={imgLazy ? "lazy" : undefined}
 				/>
