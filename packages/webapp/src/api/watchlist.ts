92026ee9b2021af6f92bce3102adca57d0b61f54
Diego Blattner <>
2025-10-27 00:39:12 +0000

    removing code

--- packages/webapp/src/api/watchlist.ts
+++ packages/webapp/src/api/watchlist.ts
@@ -2,18 +2,11 @@ import type { ApiMovie, ApiStatus } from "shared";
 
 type PostWatchlistPayload = { movie: ApiMovie; add: boolean };
 
-const url = import.meta.env.URL ?? "http://localhost";
-const port = import.meta.env.PORT ?? 5174;
-export const SITE_URL = `${url}:${port}`;
-
 export async function postWatchlist({ movie, add }: PostWatchlistPayload) {
 	const boolStr = add ? "true" : "false";
-	const response = await fetch(
-		`${SITE_URL}/api/watchlist/movies/${movie.id}/${boolStr}`,
-		{
-			method: "POST",
-		},
-	);
+	const response = await fetch(`/api/watchlist/movies/${movie.id}/${boolStr}`, {
+		method: "POST",
+	});
 	const result: ApiStatus = await response.json();
 	return result.success;
 }
